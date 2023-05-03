d6ae7230a14f2c0fcd03b530eea9cf068ff38106
JoceliaHora <joceliahora@gmail.com>
2023-05-03 15:04:07 -0300

    adicionando readme

--- algoritimos/1005.js
+++ algoritimos/1005.js
@@ -1,2 +1,11 @@
 var input = require('fs').readFileSync('./dev/stdin', 'utf8');
 var lines = input.split('\n');
+
+var A = parseFloat(lines[0]);
+var B = parseFloat(lines[1]);
+
+
+
+console.log("MEDIA = %0.2f" + (A * B) /2);
+
+//Media de notas, soma das duas notas e divide pela quantidade de vezes que foram realidazas ex?: 5, 10, >>> 15/2
